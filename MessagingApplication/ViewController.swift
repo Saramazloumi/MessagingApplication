6318cdeeab9fbb55cc97720b0c92fbfae7465e60
Sara Mazloumi <mz.sara29@gmail.com>
2019-04-19 21:48:34 -0400

    Initial Commit

---
+++ MessagingApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessagingApplication
+//
+//  Created by Sara  on 2019-04-19.
+//  Copyright © 2019 Sara Mazloumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
